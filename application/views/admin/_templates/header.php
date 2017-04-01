5b87904769de9f494b7a1c471f946a3f4e2eea95
Viral Kanzariya <viral.kreatosoft@gmail.com>
2017-04-01 20:25:12 +0530

    CSS url fixes

--- application/views/admin/_templates/header.php
+++ application/views/admin/_templates/header.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <!-- Ionicons -->
         <!--  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
         <!-- Theme style -->
-        <link rel="stylesheet" href="<?php S3_CDN . $frameworks_dir . '/adminlte/css/AdminLTE.css'; ?>">
+        <link rel="stylesheet" href="<?php echo S3_CDN . $frameworks_dir . '/adminlte/css/AdminLTE.css'; ?>">
         <!-- AdminLTE Skins. Choose a skin from the css/skins
         folder instead of downloading all of them to reduce the load. -->
         <link rel="stylesheet" href="<?php echo S3_CDN . $frameworks_dir . '/adminlte/css/skins/_all-skins.min.css'; ?>">
