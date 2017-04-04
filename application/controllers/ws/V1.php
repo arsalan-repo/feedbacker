af396b814a18a0e47d79dadd3dfa619dd79f1220
Viral Kanzariya <viral.kreatosoft@gmail.com>
2017-04-05 02:42:44 +0530

    API - Debug search results

--- application/controllers/ws/V1.php
+++ application/controllers/ws/V1.php
@@ -1325,8 +1325,8 @@ class V1 extends CI_Controller {
 			}
 			
 			// API LOG
-			$insert_array['post_request'] = $this->input->post();
-			$insert_array['json_response'] = $feedback;
+			$insert_array['post_request'] = json_encode(array('RESULT' => $this->input->post(), 'MESSAGE' => '', 'STATUS' => 0));
+			$insert_array['json_response'] = json_encode(array('RESULT' => $feedback, 'MESSAGE' => '', 'STATUS' => 0));
 			
 			$insert_result = $this->common->insert_data($insert_array, $tablename = 'api_log');
 			
