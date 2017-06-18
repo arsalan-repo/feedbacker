8da9d8f20d8d01cde0107103647b58fe60a71ca9
Viral Kanzariya <viral.kreatosoft@gmail.com>
2017-06-18 16:30:54 +0530

    Dashboard - Trends / What to Follow

--- application/models/Common.php
+++ application/models/Common.php
@@ -25,6 +25,15 @@ class Common extends CI_Model {
             return array();
         }
     }
+	
+	function limitText($text, $limit) {
+      if (str_word_count($text, 0) > $limit) {
+          $words = str_word_count($text, 2);
+          $pos = array_keys($words);
+          $text = substr($text, 0, $pos[$limit]) . '...';
+      }
+      return $text;
+    }
 
     //get user country
     function user_country($user_id) {
